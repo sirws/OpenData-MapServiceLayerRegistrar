f430e2fff75c5b2c05e4b1802f4cea3557cf4a10
Scott Moore <smoore@esri.com>
2014-09-18 14:44:32 -0700

    Updating to support projection
    This uses arcpy to project the layer extent from the layers wkid to 4326

--- registerLayers.py
+++ registerLayers.py
@@ -1,11 +1,12 @@
-import json
+import json, arcpy
 from arcrest.ags import mapservice
+from arcrest.ags import geometryservice
 from arcrest.agol import admin
 
 if __name__ == "__main__":
     try:
         #url = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/MapServer"
-        url = "{url to map service}"
+        url = "{your server URL}"
         fs = mapservice.MapService(url=url)
         for serviceLayer in fs.layers:
             if serviceLayer.type == "Feature Layer":
@@ -14,15 +15,26 @@ if __name__ == "__main__":
                 agol = admin.AGOL(username='{AGOL Username}',password='{AGOL Password}')
                 itemURL = url + "/" + str(serviceLayer.id)
                 inparams = {"url":itemURL}
-                
+
+                #in the future, I would like to support reprojecting the data using the geometry service instead of arcpy
+                #gs = geometryservice.GeometryService(url="http://utility.arcgisonline.com/arcgis/rest/services/Geometry/GeometryServer")
+
+                #create an ArcPy Extent Object so we can reproject it to 4326 (lat/long)
+                layerExtent = arcpy.Extent(serviceLayer.extent.get('xmin'),serviceLayer.extent.get('ymin'),serviceLayer.extent.get('xmax'),serviceLayer.extent.get('ymax'))
+                layerExtent.spatialReference = arcpy.SpatialReference(serviceLayer.extent["spatialReference"]["wkid"])
+
+                #project the extent of the layer to 4326
+                layerExtentPrj = layerExtent.projectAs(arcpy.SpatialReference(4326))
+
                 res = agol.addItem(name=serviceLayer.name,
-                             tags="Bellingham, Open Data, Washington",
+                             tags="Open Data, MyAgency",
                              description=serviceLayer.description,
                              snippet=serviceLayer.name,
                              data=None,
-                             extent=str(serviceLayer.extent.get('xmin')) + "," + str(serviceLayer.extent.get('ymin')) + "," + str(serviceLayer.extent.get('xmax')) + "," + str(serviceLayer.extent.get('ymax')),
+                             extent=str(layerExtentPrj.XMin) + "," + str(layerExtentPrj.YMin) + "," + str(layerExtentPrj.XMax) + "," + str(layerExtentPrj.YMax),
                              item_type="Feature Service",
-                             folder="765cc150b552475eb4f2b87808824493",
+                             #folder="765cc150b552475eb4f2b87808824493",
+                             folder=None,
                              inparams=inparams,
                              typeKeywords=["Data", "Service", "Feature Service", "ArcGIS Server", "Feature Access"]
                             )
